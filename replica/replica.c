905474b1d646a7cd3e57a3d9c58f049d5ebd8336
VladaGromova <vladagromova1106@gmail.com>
2024-10-18 15:22:31 +0200

    simple communication client-replica

--- replica/replica.c
+++ replica/replica.c
@@ -1,59 +1,270 @@
+#define _GNU_SOURCE
+#include <errno.h>
+#include <fcntl.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/epoll.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/un.h>
 #include <unistd.h>
-#include <arpa/inet.h>
 
+#ifndef TEMP_FAILURE_RETRY
+#define TEMP_FAILURE_RETRY(expression)             \
+    (__extension__({                               \
+        long int __result;                         \
+        do                                         \
+            __result = (long int)(expression);     \
+        while (__result == -1L && errno == EINTR); \
+        __result;                                  \
+    }))
+#endif
+
+#define ERR(source) (perror(source), fprintf(stderr, "%s:%d\n", __FILE__, __LINE__), exit(EXIT_FAILURE))
+#define BACKLOG 3
+#define MAX_EVENTS 16
 #define PORT 8000
+#define ADDRESS "192.168.0.38"
 #define BUFFER_SIZE 1024
 
-int main() {
-    int server_socket, client_socket;
-    struct sockaddr_in server_addr, client_addr;
-    socklen_t addr_len = sizeof(client_addr);
+volatile sig_atomic_t do_work = 1;
 
-    // Create a TCP socket
-    if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("Socket failed");
-        exit(EXIT_FAILURE);
-    }
+void sigint_handler(int sig) { do_work = 0; }
 
-    // Define the server address
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = inet_addr("192.168.0.38");;  // Bind to any available interface
-    server_addr.sin_port = htons(PORT);
+void usage(char *name) { fprintf(stderr, "USAGE: %s socket port\n", name); }
 
-    // Bind the socket to the port
-    if (bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
-        perror("Bind failed");
-        close(server_socket);
-        exit(EXIT_FAILURE);
-    }
-
-    // Listen for incoming connections
-    if (listen(server_socket, 3) < 0) {
-        perror("Listen failed");
-        close(server_socket);
-        exit(EXIT_FAILURE);
-    }
-
-    printf("Server is listening on port %d...\n", PORT);
-
-    // Accept an incoming connection
-    if ((client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &addr_len)) < 0) {
-        perror("Accept failed");
-        close(server_socket);
-        exit(EXIT_FAILURE);
-    }
-
-    // Print a message when a new client connects
-    printf("New client connected!\n");
-
-    // Close the client connection
-    close(client_socket);
-
-    // Close the server socket
-    close(server_socket);
-
+int sethandler(void (*f)(int), int sigNo)
+{
+    struct sigaction act;
+    memset(&act, 0, sizeof(struct sigaction));
+    act.sa_handler = f;
+    if (-1 == sigaction(sigNo, &act, NULL))
+        return -1;
     return 0;
+}
+
+int make_tcp_socket(void)
+{
+    int sock;
+    sock = socket(PF_INET, SOCK_STREAM, 0);
+    if (sock < 0)
+        ERR("socket");
+    return sock;
+}
+
+struct sockaddr_in make_address(char *address, char *port)
+{
+    int ret;
+    struct sockaddr_in addr;
+    struct addrinfo *result;
+    struct addrinfo hints = {};
+    hints.ai_family = AF_INET;
+    if ((ret = getaddrinfo(address, port, &hints, &result)))
+    {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(ret));
+        exit(EXIT_FAILURE);
+    }
+    addr = *(struct sockaddr_in *)(result->ai_addr);
+    freeaddrinfo(result);
+    return addr;
+}
+
+int bind_tcp_socket(int backlog_size)
+{
+    struct sockaddr_in addr;
+    int socketfd, t = 1;
+    socketfd = make_tcp_socket();
+    memset(&addr, 0, sizeof(struct sockaddr_in));
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(PORT);
+    addr.sin_addr.s_addr = inet_addr(ADDRESS);
+    if (setsockopt(socketfd, SOL_SOCKET, SO_REUSEADDR, &t, sizeof(t)))
+        ERR("setsockopt");
+    if (bind(socketfd, (struct sockaddr *)&addr, sizeof(addr)) < 0)
+        ERR("bind");
+    if (listen(socketfd, backlog_size) < 0)
+        ERR("listen");
+    return socketfd;
+}
+
+int add_new_client(int sfd)
+{
+    int nfd;
+    if ((nfd = TEMP_FAILURE_RETRY(accept(sfd, NULL, NULL))) < 0)
+    {
+        if (EAGAIN == errno || EWOULDBLOCK == errno)
+            return -1;
+        ERR("accept");
+    }
+    return nfd;
+}
+
+ssize_t bulk_read(int fd, char *buf, size_t count)
+{
+    int c;
+    size_t len = 0;
+    do
+    {
+        c = TEMP_FAILURE_RETRY(read(fd, buf, count));
+        if (c < 0)
+            return c;
+        if (0 == c)
+            return len;
+        buf += c;
+        len += c;
+        count -= c;
+    } while (count > 0);
+    return len;
+}
+
+ssize_t bulk_write(int fd, char *buf, size_t count)
+{
+    int c;
+    size_t len = 0;
+    do
+    {
+        c = TEMP_FAILURE_RETRY(write(fd, buf, count));
+        if (c < 0)
+            return c;
+        buf += c;
+        len += c;
+        count -= c;
+    } while (count > 0);
+    return len;
+}
+
+void doServer(int tcp_listen_socket)
+{
+    int epoll_descriptor;
+    if ((epoll_descriptor = epoll_create1(0)) < 0)
+    {
+        ERR("epoll_create:");
+    }
+    struct epoll_event event, events[MAX_EVENTS];
+    event.events = EPOLLIN;
+
+    event.data.fd = tcp_listen_socket;
+    if (epoll_ctl(epoll_descriptor, EPOLL_CTL_ADD, tcp_listen_socket, &event) == -1)
+    {
+        perror("epoll_ctl: listen_sock");
+        exit(EXIT_FAILURE);
+    }
+
+    int nfds;
+    char buffer[BUFFER_SIZE]; // Buffer for storing the received string
+    ssize_t size;
+    sigset_t mask, oldmask;
+    sigemptyset(&mask);
+    sigaddset(&mask, SIGINT);
+    sigprocmask(SIG_BLOCK, &mask, &oldmask);
+    while (do_work)
+    {
+        if ((nfds = epoll_pwait(epoll_descriptor, events, MAX_EVENTS, -1, &oldmask)) > 0)
+        {
+            for (int n = 0; n < nfds; n++)
+            {
+                int client_socket = add_new_client(events[n].data.fd);
+                if ((size = bulk_read(client_socket, buffer, BUFFER_SIZE - 1)) < 0)
+                    ERR("read:");
+                if (size > 0)
+                {
+                    buffer[size] = '\0';  // Null-terminate the string to make it safe
+                    printf("Received string from client: %s\n", buffer);
+                }
+                if (TEMP_FAILURE_RETRY(close(client_socket)) < 0)
+                    ERR("close");
+            }
+        }
+        else
+        {
+            if (errno == EINTR)
+                continue;
+            ERR("epoll_pwait");
+        }
+    }
+    if (TEMP_FAILURE_RETRY(close(epoll_descriptor)) < 0)
+        ERR("close");
+    sigprocmask(SIG_UNBLOCK, &mask, NULL);
+}
+
+int main() {
+    int tcp_listen_socket;
+    int new_flags;
+    if (sethandler(SIG_IGN, SIGPIPE))
+        ERR("Seting SIGPIPE:");
+    if (sethandler(sigint_handler, SIGINT))
+        ERR("Seting SIGINT:");
+    tcp_listen_socket = bind_tcp_socket(BACKLOG);
+    new_flags = fcntl(tcp_listen_socket, F_GETFL) | O_NONBLOCK;
+    fcntl(tcp_listen_socket, F_SETFL, new_flags);
+    doServer(tcp_listen_socket);
+    if (TEMP_FAILURE_RETRY(close(tcp_listen_socket)) < 0)
+        ERR("close");
+    fprintf(stderr, "Server has terminated.\n");
+    return EXIT_SUCCESS;
+
+
+    // int server_socket, client_socket;
+    // struct sockaddr_in server_addr, client_addr;
+    // socklen_t addr_len = sizeof(client_addr);
+
+    // // Create a TCP socket
+    // if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+    //     perror("Socket failed");
+    //     exit(EXIT_FAILURE);
+    // }
+
+    // // Define the server address
+    // server_addr.sin_family = AF_INET;
+    // server_addr.sin_addr.s_addr = inet_addr("192.168.0.38");
+    // server_addr.sin_port = htons(PORT);
+
+    // // Bind the socket to the port
+    // if (bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+    //     perror("Bind failed");
+    //     close(server_socket);
+    //     exit(EXIT_FAILURE);
+    // }
+
+    // // Listen for incoming connections
+    // if (listen(server_socket, 3) < 0) {
+    //     perror("Listen failed");
+    //     close(server_socket);
+    //     exit(EXIT_FAILURE);
+    // }
+
+    // printf("Server is listening on port %d...\n", PORT);
+
+    // // Accept an incoming connection
+    // if ((client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &addr_len)) < 0) {
+    //     perror("Accept failed");
+    //     close(server_socket);
+    //     exit(EXIT_FAILURE);
+    // }
+
+    // // Print a message when a new client connects
+    // printf("New client connected!\n");
+
+    // char buffer[BUFFER_SIZE] = {0};  // Buffer to hold the received message
+    // int bytes_received = recv(client_socket, buffer, BUFFER_SIZE, 0);
+    // if (bytes_received < 0) {
+    //     perror("Receive failed");
+    // } else {
+    //     buffer[bytes_received] = '\0';  // Null-terminate the received string
+    //     printf("Message received from client: %s\n", buffer);
+    // }
+
+
+    // // Close the client connection
+    // close(client_socket);
+
+    // // Close the server socket
+    // close(server_socket);
+
+    // return 0;
 }
