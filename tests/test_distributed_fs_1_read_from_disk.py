1150110b5484a02b36dd486d09a3b484c31be1d5
glowacz <glowacki.pj@gmail.com>
2024-12-19 21:56:08 +0100

    added tests for reading from client disk and connecting to master

---
+++ tests/test_distributed_fs_1_read_from_disk.py
@@ -0,0 +1,42 @@
+import subprocess
+import pytest
+import time
+import socket
+from pathlib import Path
+import shlex
+
+@pytest.fixture(scope="module")
+def setup_docker_environment():
+    script_dir = Path("../")
+    command = "./docker-restart-detached.sh docker-compose-read-from-disk.yaml"
+    args = shlex.split(command)
+
+    result = subprocess.run(
+        args,
+        cwd=script_dir,
+        capture_output=True,
+        text=True,
+        check=True
+    )
+
+    time.sleep(6)
+    
+    yield
+
+def get_logs(command):
+    args = shlex.split(command)
+    container_logs = subprocess.run(
+        args,
+        capture_output=True,
+        text=True,
+        check=True,
+    )
+    logs = container_logs.stdout
+    print("=================== Logs are ===================\n", logs)
+    return logs
+
+def test_client_output(setup_docker_environment):
+    client_logs = get_logs("docker logs distributed-fs-client_container-1")
+
+    assert "opening file: alphabet" in client_logs, "[ERR] client didn't receive/parse the file(path)"
+    assert "file size: 26 bytes" in client_logs, "[ERR] client failed in reading the file (from its disk)"
---
+++ tests/test_distributed_fs_2_connect_to_master.py
@@ -0,0 +1,49 @@
+import subprocess
+import pytest
+import time
+import socket
+from pathlib import Path
+import shlex
+
+@pytest.fixture(scope="module")
+def setup_docker_environment():
+    script_dir = Path("../")
+    command = "./docker-restart-detached.sh docker-compose-write.yml"
+    args = shlex.split(command)
+
+    result = subprocess.run(
+        args,
+        cwd=script_dir,
+        capture_output=True,
+        text=True,
+        check=True
+    )
+
+    time.sleep(6)
+    
+    yield
+
+def get_logs(command):
+    args = shlex.split(command)
+    container_logs = subprocess.run(
+        args,
+        capture_output=True,
+        text=True,
+        check=True,
+    )
+    logs = container_logs.stdout
+    print("=================== Logs are ===================\n", logs)
+    return logs
+
+def test_client_output(setup_docker_environment):
+    client_logs = get_logs("docker logs distributed-fs-client_container-1")
+    server_logs = get_logs("docker logs distributed-fs-server_container-1")
+
+    assert "opening file: alphabet" in client_logs, "[ERR] client didn't receive/parse the file(path)"
+    assert "file size: 26 bytes" in client_logs, "[ERR] client failed in reading the file (from its disk)"
+    
+    assert "Succesfully connected to 9001" in client_logs, "[ERR] client failed to connect with server"
+
+    assert "write request detected" in server_logs, "[ERR] write request from client wasn't received by master"
+    assert "fileRequestWrite->path: alphabet" in server_logs, "[ERR] master didn't receive right path"
+    assert "fileRequestWrite->size: 26" in server_logs, "[ERR] master didn't receive right size"
